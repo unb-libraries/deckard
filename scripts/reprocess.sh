b0db5b2f2edcdb0480e1ae5298bd057c739d0ff1
Jacob Sanford <jsanford@unb.ca>
2024-02-28 13:09:04 +0000

    Move scripts

--- scripts/reprocess.sh
+++ scripts/reprocess.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# rm -rf data/collectors/libpages/cache/*
+rm -rf data/collectors/libpages/cache/*
 rm -rf data/chunkers/standard/output/*
 poetry run build:rag libpages
-poetry run api
+poetry run api:start
